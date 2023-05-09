259b4a54da6882a6f1a61b40e6a6edb1aceca9c5
CarlStro <121464561+CarlStro@users.noreply.github.com>
2023-05-09 14:41:22 -0500

    Add resize button and finishing touches on basic version

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 let sideSquares = 16;
 const mainContainer = document.querySelector('.mainContainer');
+
 let grid = [];
 
-for (let i = 0; i < sideSquares; i++) {
-    let rowContainer = document.createElement('div');
-    rowContainer.classList.add('rowContainer');
+populateGrid();
+etchEvent();
 
-    for (let j = 0; j < sideSquares; j++) {
-        let square = document.createElement('div');
-        rowContainer.appendChild(square);
-        grid.push(square);
+function populateGrid(size) {
+    if (arguments.length != 0) sideSquares = size;
+    
+    for (let i = 0; i < sideSquares; i++) {
+        let rowContainer = document.createElement('div');
+        rowContainer.classList.add('rowContainer');
+    
+        for (let j = 0; j < sideSquares; j++) {
+            let square = document.createElement('div');
+            rowContainer.appendChild(square);
+            grid.push(square);
+        }
+        mainContainer.appendChild(rowContainer);
     }
-    mainContainer.appendChild(rowContainer);
 }
 
-grid.forEach((box) => {
-    box.addEventListener('mouseenter', () => {
-    box.classList.add('backGroundChange');
+function etchEvent() {
+    grid.forEach((box) => {
+        box.addEventListener('mouseenter', () => {
+        box.classList.add('backGroundChange');
+        });
     });
+}
+
+const resizeButton = document.querySelector('button');
+
+resizeButton.addEventListener('click', () => {
+    let gridSize = 0;
+
+    while (gridSize < 2 || gridSize > 100 || !Number.isInteger(gridSize)) {
+        gridSize = parseInt(prompt('Choose a side length from 2 to 100'));
+        if (gridSize == null) return;
+    }
+    
+    mainContainer.textContent = '';
+
+    grid = [];
+    populateGrid(gridSize);
+    etchEvent();
 });
