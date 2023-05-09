074764765281c3cf0cdd349df78e99f34349a14b
CarlStro <121464561+CarlStro@users.noreply.github.com>
2023-05-09 13:30:25 -0500

    Add hover effect

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let sideSquares = 16;
 const mainContainer = document.querySelector('.mainContainer');
+let grid = [];
 
 for (let i = 0; i < sideSquares; i++) {
     let rowContainer = document.createElement('div');
@@ -8,6 +9,13 @@ for (let i = 0; i < sideSquares; i++) {
     for (let j = 0; j < sideSquares; j++) {
         let square = document.createElement('div');
         rowContainer.appendChild(square);
+        grid.push(square);
     }
     mainContainer.appendChild(rowContainer);
 }
+
+grid.forEach((box) => {
+    box.addEventListener('mouseenter', () => {
+    box.classList.add('backGroundChange');
+    });
+});
